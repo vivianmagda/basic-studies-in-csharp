b94aabd2f7141834c84d64ff8838207773de6015
vivianmagda <vivianmagda@live.com>
2024-09-30 22:25:33 -0300

    for loops

--- C# Fundamentals/Section 5 Exercises/obj/Debug/net8.0/Section5Exercises.AssemblyInfo.cs
+++ C# Fundamentals/Section 5 Exercises/obj/Debug/net8.0/Section5Exercises.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Section5Exercises")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+264fb464a26c88afbd75831641b0d71d28e84e47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f83a279ed3eb9b9ab291a40dfdb4025903a58f4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Section5Exercises")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Section5Exercises")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
